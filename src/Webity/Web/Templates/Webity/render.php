94fdb343e8648308d3f9a0cbcc73c1f43dd70858
Micah Iriye <miriye@csumb.edu>
2014-08-21 09:35:51 -0700

    render in templates now calls renderMeta and renderHeader into seperate places

--- src/Webity/Web/Templates/Webity/render.php
+++ src/Webity/Web/Templates/Webity/render.php
@@ -1,8 +1,10 @@
 <?php 
 
 if ($this->document) {
+	$this->meta = $this->document->renderMeta();
 	$this->head = $this->document->renderHeader();
 } else {
+	$this->meta = '';
 	$this->head = '<title>Error generating header section</title>';
 } ?>
 
